1dfe64ead8ef92f29b8cab639cd2c9c4c1ba2d94
Sounded <146730001+roberts-pe@users.noreply.github.com>
2024-10-05 21:33:19 +0100

    Add: wave manager

--- Assets/Scripts/Nanny.cs
+++ Assets/Scripts/Nanny.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -12,29 +13,23 @@ public class Nanny : MonoBehaviour {
     [SerializeField] int[] wave;
     [SerializeField] Transform[] points;
     [SerializeField] float spawnDelay;
-    public bool waveComplete = false;
-    [SerializeField] Nanny waitSpawner;
+    public Action OnWaveComplete;
 
-    // Update is called once per frame
-    void Update() {
-        if (!waveComplete) {
-            if (waitSpawner && !waitSpawner.waveComplete) {
-                return;
-            }
-            StartCoroutine(SpawnWave());
-        }
-    }
 
     void Spawn(int EnemiesID) {
         GameObject enemyObj = Instantiate(EnemyTypes[EnemiesID], points[0].position, Quaternion.AngleAxis(Vector2.SignedAngle(Vector2.up, (points[1].position - points[0].position).normalized), Vector3.forward));
         enemyObj.GetComponent<Movement>().Locations = points;
     }
     IEnumerator SpawnWave() {
-        waveComplete = true;
         for (int i = 0; i < wave.Length; i++) {
             Spawn(wave[i]);
             yield return  Yielders.WaitForSeconds(spawnDelay);
         }
-        
+        OnWaveComplete?.Invoke();
+    }
+
+    public void StartWave()
+    {
+        StartCoroutine(SpawnWave());
     }
 }
