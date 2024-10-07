838aa4a3fb5cd79a79c4f6b86f9141f6d68b8999
Archie-Murray <76486169+Archie-Murray@users.noreply.github.com>
2024-10-07 16:57:02 +0100

    feat(main): Fixed BasicTower Projectile speed

--- Assets/Scripts/Entities/Towers/BasicTower.cs
+++ Assets/Scripts/Entities/Towers/BasicTower.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BasicTower : Tower {
     [SerializeField] string AnimName = "Hand Throw";
+    [SerializeField] private float projectileSpeed;
 
     public override void Shoot(Collider2D[] enemies) {
 
@@ -11,7 +12,7 @@ public class BasicTower : Tower {
         animator.Play(AnimName);
         GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.AngleAxis(Vector2.SignedAngle(Vector2.up, (Vector2)(enemies[0].transform.position - transform.position).normalized), Vector3.forward));
         projectile.GetOrAddComponent<AutoDestroy>().Duration = 2;
-        projectile.GetOrAddComponent<ProjectileMover>().Speed = 5;
+        projectile.GetOrAddComponent<ProjectileMover>().Speed = projectileSpeed;
         projectile.GetOrAddComponent<EntityDamager>().Init(DamageFilter.Enemy, damage);
         attackTimer.Reset(attackTime);
         attackTimer.Start();
