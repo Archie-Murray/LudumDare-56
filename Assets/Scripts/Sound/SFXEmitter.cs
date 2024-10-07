2708235ed6f7c2d1580e12ea57afd06adaa41935
Sounded <146730001+roberts-pe@users.noreply.github.com>
2024-10-07 13:30:04 +0100

    Add: button click

--- Assets/Scripts/Sound/SFXEmitter.cs
+++ Assets/Scripts/Sound/SFXEmitter.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 using Utilities;
 
-public enum SoundEffectType { None, Shoot, Hit, Death, TowerPlace }
+public enum SoundEffectType { None, Shoot, Hit, Death, TowerPlace,UIClick }
 
 public class SFXEmitter : MonoBehaviour {
     [SerializeField] private Dictionary<SoundEffectType, AudioSource> _sources;
